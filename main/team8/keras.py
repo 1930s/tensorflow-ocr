31de5a3c0a2317554dddbfbb8cf0b88d8d204901
Blake <bllpr15@gmail.com>
2018-09-30 22:33:09 -0400

    began simple tensorflow with keras

--- main/team8/keras.py
+++ main/team8/keras.py
@@ -1,2 +1,13 @@
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow import keras #library for creating datasets and training programs
+import numpy as np
+import matplotlib.pyplot as plt #can be used to show a visual representation of our data as a plot
+import string
+
+#open file here to begin implementing data
+ocr_file = 'temp.data' 
+#we would pull the file based on which type of command they used in the makefile
+#this means the file for bashevis text would be labeled bashevis.data and we can set ocr_file to be that
+with open(ocr_file, 'r') as f: #open with read privileges
+  #insert code to use ocr_file
+
