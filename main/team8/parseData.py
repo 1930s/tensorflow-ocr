7c98ddeeef30de6a4a503df37e80547f691fc680
Blake Price <blpr226@cs.uky.edu>
2018-10-24 12:33:16 -0400

    tupleArray bug including everything into every array of the arrays

--- main/team8/parseData.py
+++ main/team8/parseData.py
@@ -9,22 +9,25 @@ def file_len(fname):
 
 def parseFile(filename):
   file = open(filename, "r")
-  fileLen = file_len(filename)
+  fileLen = file_len(filename)  #get length of the file
+  lineLen = len(file.readline()) #get length of each line
+  valDivisor = 6
+  glyphLen = (lineLen/valDivisor)+1 #get glyph value length +1 for all 28 values
+  tupleArray = [[0]*glyphLen]*fileLen #set dimensions of array to hold values
 
-  tupleArray = [[0]*28]*fileLen
   lineCounter = 0
   for line in file:
     #print line + "\n"
     start = 0 #reset start of line range
     for digit in range(0, len(line)):
-      if(line[digit] == " " and start <= digit): #look for a divider
-        tupleArray[lineCounter][int(math.floor(digit/5))-1] = line[start:digit] #insert new value
-        start = digit #set previous line range to previous divider
+      if(line[digit] == " " and start <= digit and line[digit] != "\n"): #look for a divider
+        tupleArray[lineCounter][int(math.floor(digit/valDivisor))] = line[start:digit] #insert new value to tupleArray[0][0] for example
+        start = digit+1 #set previous line range to previous divider, +1 so its not on the " "
     lineCounter += 1 #we are on the next line for reading
     #tupleArray = line
     #print(tupleArray)
 
-    return tupleArray
+  return tupleArray
 
 def main():
   #print(sys.argv[1])
