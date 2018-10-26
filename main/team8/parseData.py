c7a2772340f23cd6585457a717d4f2b757c0cde7
Blake Price <blpr226@cs.uky.edu>
2018-10-26 11:31:59 -0400

    parseData finished (hopefully)

--- main/team8/parseData.py
+++ main/team8/parseData.py
@@ -1,33 +1,22 @@
 import sys
-import math
 
-def file_len(fname):
-  with open(fname) as f:
-    for i, l in enumerate(f):
-      pass
-    return i+1
-
+#simply goes through the .data file and pushes each glyph into tuples
 def parseFile(filename):
   file = open(filename, "r")
-  fileLen = file_len(filename)  #get length of the file
-  lineLen = len(file.readline()) #get length of each line
-  valDivisor = 6
-  glyphLen = (lineLen/valDivisor)+1 #get glyph value length +1 for all 28 values
-  #tupleArray = [[0]*glyphLen]*fileLen #set dimensions of array to hold values
   tupleArray = []
-  tupleArray.append([])
 
   lineCounter = 0
   for line in file:
-    #print line + "\n"
+    tupleArray.append([])
     start = 0 #reset start of line range
+    
     for digit in range(0, len(line)):
-      if(line[digit] == " " and start <= digit and line[digit] != "\n"): #look for a divider
-        tupleArray[lineCounter].append(line[start:digit])
+      if(line[digit] == " " and start <= digit or line[digit] == "\n"): #look for a divider
+        tupleArray[lineCounter].append(line[start:digit]) #adds value into the tuple
         #tupleArray[lineCounter][int(math.floor(digit/valDivisor))] = line[start:digit] #insert new value to tupleArray[0][0] for example
         start = digit+1 #set previous line range to previous divider, +1 so its not on the " "
+    
     lineCounter += 1 #we are on the next line for reading
-    tupleArray.append([])
 
   return tupleArray
 
