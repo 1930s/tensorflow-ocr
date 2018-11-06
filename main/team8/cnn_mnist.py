a3e834b91d1eaeb9da941acaadcaa62a60efc5e2
Blake Price <blpr226@cs.uky.edu>
2018-11-06 17:50:32 -0500

    Finished functionality for cnn_mnist.py, ready for testing and then adjustments

--- main/team8/cnn_mnist.py
+++ main/team8/cnn_mnist.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-# Our application logic will be added here
+# PREDICTION and TRAINING
 def cnn_model_fn(features, labels, mode):
   """Model function for CNN."""
   # Input Layer
@@ -49,8 +49,9 @@ def cnn_model_fn(features, labels, mode):
       # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
       # `logging_hook`.
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-  }
+      }
 
+  # Predictions based on input tensor(s)
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
@@ -68,9 +69,56 @@ def cnn_model_fn(features, labels, mode):
   # Add evaluation metrics (for EVAL mode)
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
+        labels=labels, predictions=predictions["classes"])}
   return tf.estimator.EstimatorSpec(
-      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+# MAIN
+def main(unused_argv):
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+
+  # Stores the training feature data (raw images/values for training purposes)
+  train_data = mnist.train.images # Returns np.array
+
+  # Carries the labels for the language being used (example: a-z or 0-9)
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+
+  # Similar to above but evaluation feature data
+  eval_data = mnist.test.images # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+
+  # Create the Estimator
+  mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+
+  # Set up logging for predictions
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
+
+
+  # Train the model
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": train_data}, 
+      y=train_labels, 
+      batch_size=100, 
+      num_epochs=None, 
+      shuffle=True)
+  mnist_classifier.train(
+    input_fn=train_input_fn,
+    steps=20000,
+    hooks=[logging_hook])
+
+
+  # Evaluate the model and print results
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": eval_data},
+    y=eval_labels,
+    num_epochs=1,
+    shuffle=False)
+  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
 
 if __name__ == "__main__":
   tf.app.run()
