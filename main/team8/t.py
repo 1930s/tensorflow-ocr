9503c30603d738d3f37b15ce301a2b76a33e0484
AtanasGolev <abgolev@gmail.com>
2018-11-10 21:35:08 -0500

    playing with parameters in t.py

--- main/team8/t.py
+++ main/team8/t.py
@@ -3,6 +3,8 @@ from tensorflow import keras
 
 import tensorflow as tf
 import numpy as np
+import numbers
+import decimal
 
 #HelloWorld
 hello = tf.constant('Hello, TensorFlow!')
@@ -12,7 +14,9 @@ print(sess2.run(hello))
 ###############################################
 
 #Leah's code here
-file = open("../fontData/kafka.data", "r")
+#file = open("../fontData/kafka.data", "r")
+filename = './kafka2.data'
+file = open(filename, "r")
 labels_arr = []
 lineCounter = 0
 
@@ -25,11 +29,11 @@ for line in file:
       labels_arr.append(letter)
   lineCounter += 1 #we are on the next line for reading
 
-print(labels_arr)
+#print(labels_arr)
 remove_dups_labels_arr = set(labels_arr)
 #print(remove_dups_labels_arr)
 sorted_labels_arr = sorted(remove_dups_labels_arr)
-print(sorted_labels_arr)
+#print(sorted_labels_arr)
 
 numeric_labels_arr = []  #label input into tensyflow
 
@@ -40,9 +44,10 @@ for value in labels_arr:
             numeric_labels_arr.append(index)
 
 flowy_labels = np.array(numeric_labels_arr)
-print(flowy_labels)
-print("Shape: ")
-print(flowy_labels.shape)
+#print(flowy_labels)
+#print("Shape: ")
+#print(flowy_labels.shape)
+
 ###############################################
 
 #filenames = tf. train.string_input_producer(["./k3.csv"]);
@@ -53,7 +58,8 @@ print(flowy_labels.shape)
 
 #Blake's code
 #simply goes through the .data file and pushes each glyph into tuples
-filename = '../fontData/kafka.data'
+#filename = '../fontData/kafka.data'
+filename = './kafka2.data'
 file = open(filename, "r")
 
 tupleArray = []
@@ -66,60 +72,96 @@ for line in file:
     for digit in range(0, len(line)-1):
       if(line[digit] == " "  and start <= digit or line[digit] == "\n"): #look for a divider
         tupleArray[lineCounter].append(line[start:digit]) #adds value into the tuple
-        #tupleArray[lineCounter][int(math.floor(digit/valDivisor))] = line[start:digit] #insert new value to tupleArray[0][0] for example
         start = digit+1 #set previous line range to previous divider, +1 so its not on the " "
 
     lineCounter += 1 #we are on the next line for reading
 
 floaty_floats = np.array(tupleArray)
-print(floaty_floats)
-print("Shape: ")
-print(floaty_floats.shape)
+#print("len and other len:")
+#print(tupleArray)
+#print(len(tupleArray))
+#print(len(tupleArray[0]))
+#print("random sample")
+#print(tupleArray[5][5])
+#print([isinstance(tupleArray[5][5], numbers.Number) for x in (0, 0.0, 0j, decimal.Decimal(0))])
+#print(floaty_floats)
+#print("Shape: ")
+#print(floaty_floats.shape)
 
 ###############################################
 
+filename_test = '../tmp.out'
+file_test = open(filename_test, "r")
+
+linecount = len(open(filename_test).readlines( ))
+
+testmatrix = np.zeros(shape=(linecount, 27))
+
+rowcount = 0
+for line in file_test:
+  colcount = 0
+  for place in range(0, len(line)):
+    if(line[place]==","):
+      testmatrix[rowcount][colcount]=line[(place-8):place]
+      #testmatrix[rowcount][colcount]=colcount
+      colcount+=1
+  rowcount+=1
+
+print("Test Matrix gets tested here")
+print(len(testmatrix))
+print(len(testmatrix[0]))
+print("random sample")
+print(testmatrix[5][5])
+print([isinstance(testmatrix[5][5], numbers.Number) for x in (0, 0.0, 0j, decimal.Decimal(0))])
+
+###############################################
 # Default values, in case of empty columns. Also specifies the type of the
 # decoded result.
 
-
-#r = [[1.0]]*27;
-#col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, col25,  col26, col27 = tf.decode_csv(value, record_defaults=r, field_delim=' ')
-#features = tf.stack([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, col25,  col26, col27])
 
 with tf.Session() as sess:
   # Start populating the filename queue.
 
     model = keras.Sequential([
-      keras.layers.Dense(131072, input_shape=(27,), activation=tf.nn.relu), 
-      keras.layers.Dense(58, activation=tf.nn.softmax), #first parameter is hardcoded as # of unique labels
+      #keras.layers.Dense(131072, input_shape=(27,), activation=tf.nn.relu), 
+      keras.layers.Dense(256, input_shape=(27,), activation=tf.nn.relu), 
+      keras.layers.Dense(len(sorted_labels_arr), activation=tf.nn.softmax), #first parameter is hardcoded as # of unique labels
     ])
 
     model.compile(optimizer=tf.train.AdamOptimizer(), 
+    #model.compile(optimizer='rmsprop', 
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-    model.fit(floaty_floats, flowy_labels, epochs=20)
+    model.fit(tupleArray, flowy_labels, epochs=100)
 
-    test_loss, test_acc = model.evaluate(floaty_floats, flowy_labels)
+    print("Flowy labels:")
+    print(flowy_labels)
+
+    test_loss, test_acc = model.evaluate(tupleArray, flowy_labels)
 
     print('Test accuracy:', test_acc)
 
+    meow = model.predict(testmatrix)
+
+    #print(flowy_labels)
+    #print(testmatrix[0])
+    #print(testmatrix[1])
+    #print(testmatrix[2])
+    #print(testmatrix[3])
+    #print(testmatrix[4])
+
+    print(sorted_labels_arr)
+
+    for i in range(0,100):    
+      #print(meow[i])
+      #print(i)
+      #print("Argmax:", np.argmax(meow[i]))
+      #print("corresponding label: ")
+      print(sorted_labels_arr[np.argmax(meow[i])])
 
 
-#  coord = tf.train.Coordinator()
-#  threads = tf.train.start_queue_runners(coord=coord)
+#    print(meow[777])
+ #   print("Meow 4:", np.argmax(meow[777]))
 
-#  for i in range(50):
-    # Retrieve a single instance:
-#    example, label = sess.run([features, col27]) #change col27 with a different list of the labels
-
-#  coord.request_stop()
-#  coord.join(threads)
-
-#  print("rly go fuck yourself")
-
-
-
-
-
-
+    print(meow.shape)    
